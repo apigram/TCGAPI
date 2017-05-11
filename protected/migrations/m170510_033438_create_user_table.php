003d0f9a2a458470fb109301c3b63bea89354655
andrew <andrew.pigram@fivium.com.au>
2017-05-11 15:21:50 +1000

    Replaced personal admin user with generic admin user.

--- protected/migrations/m170510_033438_create_user_table.php
+++ protected/migrations/m170510_033438_create_user_table.php
@@ -2,33 +2,33 @@
 
 class m170510_033438_create_user_table extends CDbMigration
 {
-	public function up()
-	{
-		
-	}
+    public function up()
+    {
 
-	public function down()
-	{
-		
-	}
+    }
 
-	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
-	{
-		$this->createTable('tcg_users', array(
-				'id' => 'pk',
-				'username' => 'varchar(20) NOT NULL',
-				'password' => 'text NOT NULL',
-		), 'ENGINE=InnoDB');
-		
-		$this->insert('tcg_users', array(
-			'username' => 'apigram',
-			'password' => password_hash('fivium12', PASSWORD_DEFAULT),
-		));
-	}
+    public function down()
+    {
 
-	public function safeDown()
-	{
-		$this->dropTable('tcg_users');
-	}
+    }
+
+    // Use safeUp/safeDown to do migration with transaction
+    public function safeUp()
+    {
+        $this->createTable('tcg_users', array(
+            'id' => 'pk',
+            'username' => 'varchar(20) NOT NULL',
+            'password' => 'text NOT NULL',
+        ), 'ENGINE=InnoDB');
+
+        $this->insert('tcg_users', array(
+            'username' => 'admin',
+            'password' => password_hash('admin', PASSWORD_DEFAULT),
+        ));
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('tcg_users');
+    }
 }
