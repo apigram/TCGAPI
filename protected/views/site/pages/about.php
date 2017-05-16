488a79a2cd3620ce316c30feb2f96e6f6cf6d9dd
andrew <andrew.pigram@fivium.com.au>
2017-05-16 14:06:05 +1000

    Updated .gitignore file to ignore all IDE-specific and unversioned files. Consolidated all production-ready code. Changed all tabs to spaces.

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -1,14 +1,15 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name . ' - About';
-$this->breadcrumbs=array(
-	'About',
+$this->pageTitle = Yii::app()->name . ' - About';
+$this->breadcrumbs = array(
+    'About',
 );
 ?>
 <h1>About</h1>
 
 <p>
-  The Pokemon Trading Card Game Collection API is a RESTful microservice that allows third-party applications to store card collections in the cloud, where they can be viewed and assembled into decks.
+  The Pokemon Trading Card Game Collection API is a RESTful microservice that allows third-party applications to store
+  card collections in the cloud, where they can be viewed and assembled into decks.
   Registered users can invoke actions on the microservice using generated API keys for per-client authentication.
 </p>
